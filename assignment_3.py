14fe7a819e7368f787463d3d304c916ac743f8f1
MaudSchuurmans35 <m.m.a.schuurmans@student.tue.nl>
2024-12-11 09:34:25 +0100

    versie 1

--- assignment_3.py
+++ assignment_3.py
@@ -1,1 +1,18 @@
+import pandas as pd
+import numpy as np
+from rdkit import Chem
+from rdkit.Chem import Descriptors
 
+data = pd.read_csv('train.csv')
+print(data.shape)
+
+
+list = np.array(0)
+
+for i in range(10):
+    smile = data.iloc[i,0]
+    m = Chem.MolFromSmiles(smile)
+    vals = Descriptors.CalcMolDescriptors(m)
+    list.append(vals)
+
+print(list)
