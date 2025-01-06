b4b7c0cbad87dfe2ac609ea1799a7d413adf6884
ajwklepper <a.j.w.klepper@student.tue.nl>
2025-01-06 22:50:41 +0100

    run code is updated na naamsverandering van met_nn

--- Run Code.py
+++ Run Code.py
@@ -1,2 +1,2 @@
-from met_nn import run_code
+from final_main_assignment import run_code
 run_code(manner = 'short', fingerprintcount = 4096, augment_data_x_times = 5, write_to_file = False, train_nn_model = False)
